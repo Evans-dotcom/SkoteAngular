28b139e3dfa358ba6262bb4f1fb5f60d03e37ccb
Evans-dotcom <evaslangat@gmail.com>
2024-09-17 13:28:37 +0300

    initial commit

---
+++ Starterkit/src/app/layouts/layout.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { LayoutComponent } from './layout.component';
+import '@types/jasmine';
+
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+function beforeEach(arg0: (done: any) => any) {
+  throw new Error('Function not implemented.');
+}
